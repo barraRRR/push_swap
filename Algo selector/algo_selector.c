918d11802b9fdd647ae6845d27008e07bdefce7e
edsole-a <edsole-a@student.42madrid.com>
2026-02-11 18:36:48 +0100

    Primer draft del selector de algoritmos

---
+++ Algo selector/algo_selector.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algo_selector.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edsole-a <edsole-a@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 17:22:39 by edsole-a          #+#    #+#             */
+/*   Updated: 2026/02/11 17:31:14 by edsole-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void algo_selector(t_lst **a, t_lst **tail_a)
+{
+	float	disorder;
+
+	disorder = compute_disorder(a);
+	if (disorder < 0.2)
+		low(a, tail_a);
+	else if (0.2 <= disorder && disorder < 0.5)
+		medium(a, tail_a);
+	else
+		high(a, tail_a);
+	return;
+}
