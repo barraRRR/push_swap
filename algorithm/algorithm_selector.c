099b9f6810f5a0bb9e543dfa1e8736cc777cb2ca
edsole-a <edsole-a@student.42madrid.com>
2026-02-13 18:42:41 +0100

    Draft of the algo selector. To be modified with correct algo's names  if needed

---
+++ algorithm/algorithm_selector.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm_selector.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edsole-a <edsole-a@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 17:22:39 by edsole-a          #+#    #+#             */
+/*   Updated: 2026/02/13 18:41:10 by edsole-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+// A modificar en función de la implementación de los algoritmos en sí
+
+void algo_selector(t_lst **a, t_lst **tail_a)
+{
+	float	disorder;
+
+	disorder = compute_disorder(a);
+	if (disorder < 0.2)
+		simple(a, tail_a);
+	else if (0.2 <= disorder && disorder < 0.5)
+		medium(a, tail_a);
+	else
+		complex(a, tail_a);
+	return;
+}
