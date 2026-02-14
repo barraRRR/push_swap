e6a493423ae7f019b5f65a14ec21e47b40f67deb
Javi Barreiro <235066522+barraRRR@users.noreply.github.com>
2026-02-14 18:14:42 +0100

    submit: Makefile done . Needed for cool tester

--- check_lines.sh
+++ check_lines.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-LINES=$1
+# Parámetro de entrada
+N=$1
 
-shuf -i 0-9999 -n "$LINES" > arg.txt ; ./push_swap $(cat arg.txt) | wc -l
+# Definición de colores
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color (Reset)
+
+# Mensaje con color
+echo -e "${YELLOW}Running tester for $N numbers...${NC}"
+
+# Generar números, guardarlos y ejecutar push_swap
+# Nota: Usamos $(cat arg.txt) con precaución por el límite de argumentos
+shuf -i 0-9999 -n "$N" > arg.txt
+./push_swap $(cat arg.txt) | wc -l
